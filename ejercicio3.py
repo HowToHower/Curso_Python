9c23ba6142c8e8df686035a22db2e84075ff8de1
HowToHower <howerddo@gmail.com>
2026-01-28 23:46:42 -0300

    Auto-sync: 28-01-2026 23:46:42,33

---
+++ ejercicio3.py
@@ -0,0 +1,42 @@
+nombre_astronauta = "Hower Pozo"
+edad_astronauta = 26
+destino="La Luna"
+combustible= 450 
+velocidad = 1500
+
+print("Diario de un Astronauta\n")
+
+print(f"Hola,soy {nombre_astronauta}, tengo {edad_astronauta} años y mi próximo destino es {destino}.\n")
+
+print(f"Estoy navegando a {velocidad} km/s con {combustible}% de combustible restante hacia {destino}.\n")
+ 
+print("Fecha: 2024-01-10")
+print("Hoy experimentamos con el cultivo de plantas en microgravedad.")
+print("Mensaje personal: ¡Es increíble ver cómo crecen las lechugas aquí arriba!\n")
+ 
+print("Fecha: 2024-01-11")
+print("Realizamos una caminata espacial para reparar un panel solar.")
+print("Mensaje personal: Flotar en el espacio nunca deja de asombrarme.\n")
+
+
+
+print("|")
+print("|")
+print("|")
+print("|")
+print("|")
+print("|")
+
+print("Hola,Soy",nombre_astronauta,"Tengo",edad_astronauta,"años y mi proximo destino es",destino)
+
+
+
+print("estoy navegando a",velocidad,"Km/s con",combustible,"% de combustible restante hacia",destino)
+
+
+
+
+
+
+
+
