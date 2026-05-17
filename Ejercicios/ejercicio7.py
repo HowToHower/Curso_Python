b2801d63a1bf743bd43a3a934566616d60120ea4
HowToHower <howerddo@gmail.com>
2026-05-17 00:41:21 -0400

    Actualización automática 2026-05-17 00:41:21

---
+++ Ejercicios/ejercicio7.py
@@ -0,0 +1,7 @@
+def organizar_fiesta(invitados,tema="python",lugar="Aula de informatica"):
+        print(f"preparando una fiesta para {invitados} invitados ")
+        print(f"Tema de la fiesta: {tema}")
+        print(f"Lugar de la celebración: {lugar}")
+        
+        
+organizar_fiesta(20,"Matematicas","mi casa")# ejercicio7.py
---
+++ Ejercicios/ejercicio8.py
@@ -0,0 +1,8 @@
+# ejercicio8.py
+def generar_mensaje(nombre,mensaje="Bienvenido al curso de Python"):
+    """Documentacion de la funcion
+    esta es una funcion para crear mensajes personalizados con dos argumentos el primero para el nombre y el segundo por defecto."""
+    return f"Hola {nombre} {mensaje}"
+    
+    
+print (generar_mensaje("Hower"))
